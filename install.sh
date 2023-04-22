293c8474bb75b9f7ff94d7d62ccf04a242c5aae4
JnathanCarter <jcarter20@stu.jsu.edu>
2023-04-22 01:45:11 -0500

    add install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+LIST_OF_APPS="i3 vim tmux ninja-build gettext cmake unzip curl git steghide"
+
+sudo apt update
+sudo apt install -y $LIST_OF_APPS
+
+
+#Clone, Build, and Install Neovim
+git clone https://github.com/neovim/neovim
+cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
+
+
+
+#Clone, and Install Neovim Config
+git clone https://github.com/JnathanCarter/Nvim-Config.git ~/.config/nvim
+
+
