bb5cb61025d9780a6c2c15261d79a5978305ffa3
JnathanCarter <jcarter20@stu.jsu.edu>
2023-04-22 01:52:03 -0500

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DotFiles
+This Repository hold some of my configuration files,
+as well an installation script.
+This installation script will install some essential packges, build, install, and configure neovim,
+and will configure tmux 
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#Update and Install Apt Packages
 LIST_OF_APPS="i3 vim tmux ninja-build gettext cmake unzip curl git steghide"
 
 sudo apt update
@@ -10,9 +11,8 @@ git clone https://github.com/neovim/neovim
 cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
 
-
-
 #Clone, and Install Neovim Config
 git clone https://github.com/JnathanCarter/Nvim-Config.git ~/.config/nvim
 
-
+#Install Tmux Config
+cp .tmux.conf ~
