1d12a8881a9db804f428704987eaaa4f7243be37
JnathanCarter <jcarter20@stu.jsu.edu>
2023-04-25 15:20:49 -0500

    fix installer

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ sudo apt install -y $LIST_OF_APPS
 
 
 #Clone, Build, and Install Neovim
-git clone https://github.com/neovim/neovim ~/Desktop/
-cd ~/Desktop/neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
-cd ~/Desktop/build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
+git clone https://github.com/neovim/neovim 
+cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
 
 #Clone, and Install Neovim Config
 git clone https://github.com/JnathanCarter/Nvim-Config.git ~/.config/nvim
